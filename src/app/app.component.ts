c91964934214a702eb127ca5895b82ddce65b26e
Deepak Arya <deepakarya0155@gmail.com>
2023-01-08 20:32:42 +0530

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'TestA';
+
+  constructor(private http: HttpClient) {}
+
+  sendGet = () => {
+    console.log('a');
+    const res1 = this.http
+      .get('https://3.89.254.248:8080/ns/api/appDetails/152')
+      .subscribe((res) => {
+        console.log(res);
+      });
+    console.log(res1);
+  };
+
+  sendPost = () => {
+    console.log('post');
+    const res1 = this.http
+      .post('https://3.89.254.248:8080/ns/login', {
+        email: 'DeepakArya0155@gmail.com',
+        password: 'Arya$786',
+      })
+      .subscribe((res) => {
+        console.log(res);
+      });
+    console.log(res1);
+  };
 }
